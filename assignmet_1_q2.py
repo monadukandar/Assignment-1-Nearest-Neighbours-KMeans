3d1429283613a2e93045288581a9f60a9a1b7704
monadukandar <104590400+monadukandar@users.noreply.github.com>
2022-07-11 00:01:37 +0530

    Update assignmet_1_q2.py

--- assignmet_1_q2.py
+++ assignmet_1_q2.py
@@ -1,3 +1,4 @@
+Predicting Price of House using KNearestNeighbour Regressor  collab link : "https://colab.research.google.com/drive/1xgiPd38tqehDnetZUlLpELU9lVmO1NSB?usp=sharing"
 
-Cluster similar Houses : https://colab.research.google.com/drive/1GSvOKTbM7Imfw5AOmOifixVBrogcPeea?usp=sharing
+Cluster similar Houses collab link : https://colab.research.google.com/drive/1GSvOKTbM7Imfw5AOmOifixVBrogcPeea?usp=sharing
 
